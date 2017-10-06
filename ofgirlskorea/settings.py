31adc0458220dc5016d84921529a875fb4341367
codingbowoo <codingbowoo@gmail.com>
2017-10-06 20:25:12 +0900

    fix static folder setting

--- ofgirlskorea/settings.py
+++ ofgirlskorea/settings.py
@@ -121,4 +121,8 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = [
+    os.path.join(BASE_DIR, 'bamboo', 'static'),
+    os.path.join(BASE_DIR, 'report', 'static'),
+    ]
+
