b80e58d158b58d5bc16a83bea4d5048b7c2d5418
codingbowoo <codingbowoo@gmail.com>
2017-10-07 00:21:48 +0900

    fix STATIC root in settings.py

--- ofgirlskorea/settings.py
+++ ofgirlskorea/settings.py
@@ -119,7 +119,10 @@ USE_TZ = False
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
+STATIC_URL = '/static/'
+STATIC_ROOT = [os.path.join(BASE_DIR, 'static')]
 
+'''
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
@@ -128,3 +131,4 @@ STATICFILES_DIRS = (
 )
 
 STATIC_ROOT = ''
+'''
