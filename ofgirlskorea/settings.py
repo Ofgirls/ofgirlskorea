94978014bd794d7f760c26cd7ba0385ac33e90f3
Dohyun Kim <dhdh6190@gmail.com>
2017-10-08 01:22:11 +0900

    collectstatic이 안되어서 setting.py의 주석을 지움.

--- ofgirlskorea/settings.py
+++ ofgirlskorea/settings.py
@@ -128,10 +128,7 @@ USE_TZ = False
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-'''
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'bamboo', 'static'),
     os.path.join(BASE_DIR, 'report', 'static'),
 )
-'''
-
