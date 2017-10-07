6c1351b482e83acd981c50906f416dc909daab9a
Cloud User <centos@ip-172-31-12-205.ap-northeast-2.compute.internal>
2017-10-07 08:14:44 +0000

    reboot server

--- ofgirlskorea/settings.py
+++ ofgirlskorea/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import pymysql
+
+pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -79,8 +82,12 @@ WSGI_APPLICATION = 'ofgirlskorea.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Ofgirlskorea',
+	'USER': 'root',
+	'PASSWORD': 'Ofgirlskorea!',
+	'HOST': 'localhost',
+	'PORT': '3306',
     }
 }
 
@@ -120,15 +127,12 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 STATIC_URL = '/static/'
-STATIC_ROOT = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 '''
-STATIC_URL = '/static/'
-
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'bamboo', 'static'),
     os.path.join(BASE_DIR, 'report', 'static'),
 )
-
-STATIC_ROOT = ''
 '''
+
