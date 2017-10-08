d01e75259d3c108839eecb579f2032af400527c0
codingbowoo <codingbowoo@gmail.com>
2017-10-08 18:14:09 +0900

    Create model Comment

--- bamboo/admin.py
+++ bamboo/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 
 
 # Register your models here.
+
 admin.site.register(Post)
+admin.site.register(Comment)
--- bamboo/models.py
+++ bamboo/models.py
@@ -17,3 +17,22 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+        'bamboo.Post',
+        on_delete=models.CASCADE,
+        related_name='comments'
+    )
+    author = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    approved_comment = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+
+    def __str__(self):
+        return self.text
