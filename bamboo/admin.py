ac202d7ca13d8c8b3858f1c7eb4009c1768992c6
codingbowoo <codingbowoo@gmail.com>
2017-10-12 23:51:36 +0900

    customizing admin page

--- bamboo/admin.py
+++ bamboo/admin.py
@@ -3,6 +3,20 @@ from .models import Post, Comment
 
 
 # Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['published_date']}),
+        ('Post 내용', {'fields': ['writer', 'title', 'text']}),
+    ]
+    list_display = ('title', 'published_date')
+    list_filter = ['published_date']
+    # 좋아요 개수, 코멘트 개수로도 필터할거임
 
-admin.site.register(Post)
-admin.site.register(Comment)
+
+class CommentAdmin(admin.ModelAdmin):
+    fields = ['created_date', 'author', 'text', ]
+    # post의 pk값도 보여줘야겠음
+    # 얘도 좋아요 수로 필터링할 예정
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Comment, CommentAdmin)
